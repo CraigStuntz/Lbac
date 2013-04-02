3793384e39efd3d832235d9465feffcae836f527
CraigStuntz <craig_stuntz@acm.org>
2013-04-01 23:07:50 -0400

    Get rid of VS 2010 hack for F# tests.

---
+++ Lbac.Tests/Part01Tests.fs
@@ -0,0 +1,20 @@
+﻿namespace Lbac.Tests
+
+    open Microsoft.VisualStudio.TestTools.UnitTesting
+    open System.IO
+    open System.Text
+    open Lbac
+
+    [<TestClass>]
+    type CradleTests() = class
+        [<TestMethod>]
+        member x.testInit() = 
+            let actual = new StringBuilder()
+
+            let cradle = new Cradle(new StringReader("1"))
+            cradle.compile() |> ignore
+    end
+
+
+
+
