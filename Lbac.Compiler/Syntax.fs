41b8a55c7b589e87ec68e1824295cf9b96026a48
CraigStuntz <craig_stuntz@acm.org>
2013-04-10 23:59:12 -0400

    Add parse result type

--- Lbac.Compiler/Syntax.fs
+++ Lbac.Compiler/Syntax.fs
@@ -22,7 +22,11 @@
         | AddOp of Expr * Term
         | Term of Term
 
+    type Parsed =
+        | Some of Expr
+        | Error of string
+
     let parse(tokens: seq<Token>) =
         let expr tokens = 
-            Term(Factor(Digit(1)))
+            Some(Term(Factor(Digit(1))))
         expr tokens 
--- Lbac.Tests/SyntaxTests.fs
+++ Lbac.Tests/SyntaxTests.fs
@@ -9,7 +9,7 @@ open Syntax
 type SyntaxTests() = 
     [<TestMethod>]
     member x.``should parse 11 + 22`` () = 
-        let expected = Expr.AddOp(Expr.Term(Factor(Digit(11))), Factor(Digit(22)))
+        let expected = Some(Expr.AddOp(Expr.Term(Factor(Digit(11))), Factor(Digit(22))))
         let input = [Number(11); Symbol('+'); Number(22)]
         let actual = Syntax.parse(input)
         Assert.AreEqual(expected, actual)
