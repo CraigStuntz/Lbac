eedf9302dbc6044a6e4afeb9a717d8aeff345094
CraigStuntz <craig_stuntz@acm.org>
2013-04-11 00:04:50 -0400

    Add factor method

--- Lbac.Compiler/Syntax.fs
+++ Lbac.Compiler/Syntax.fs
@@ -27,6 +27,10 @@
         | Error of string
 
     let parse(tokens: seq<Token>) =
+        let factor = function
+            | Number n :: ts -> Some(Expr.Term(Factor(Digit(n))))
+            | _ -> Error("Number expected") 
+
         let expr tokens = 
             Some(Term(Factor(Digit(1))))
         expr tokens 
