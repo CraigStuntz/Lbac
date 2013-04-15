a1b38effe9da3459696115d0e8ba503789767d33
CraigStuntz <craig_stuntz@acm.org>
2013-04-14 23:57:03 -0400

    Add new code generator
    AST to IL codegen

---
+++ Lbac.Compiler/CodeGenerator.fs
@@ -0,0 +1,24 @@
+﻿module CodeGenerator
+
+    open IL
+    open Syntax
+
+    let private codegen_oper = function
+        | Add -> instruction.Add
+        | Subtract -> instruction.Sub
+        | Multiply -> instruction.Mul
+        | Divide -> instruction.Div
+
+    let rec codegen = function
+        | Number n -> 
+            match n with
+            | 0 -> [Ldc_I4_0]
+            | _ -> [Ldc_I4 n]
+        | Binary (lhs, oper, rhs) -> 
+            let lhsIl = codegen lhs
+            let rhsIl = codegen rhs
+            let operInst = codegen_oper oper
+            List.concat [ lhsIl; rhsIl; [operInst] ]
+
+        
+
---
+++ Lbac.Tests/CodeGeneratorTests.fs
@@ -0,0 +1,18 @@
+﻿namespace Lbac.Tests 
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open CodeGenerator
+open IL
+open Syntax
+
+[<TestClass>]
+type CodeGeneratorTests() = 
+    [<TestMethod>]
+    member x.``should codegen 1 + 2 * 0`` () = 
+        let testVal = Expr.Binary(Expr.Binary(Expr.Number(1), Add, Expr.Number(2)), Multiply, Expr.Number(0))
+        let expected = [Ldc_I4(1); Ldc_I4(2); instruction.Add; Ldc_I4_0; Mul]
+        let actual = CodeGenerator.codegen(testVal)
+        Assert.AreEqual(expected, actual)
+
+
