1b5d5cdbdcfd0ef16dc8308150955489c806eb52
Craig Stuntz <craig_stuntz@acm.org>
2013-06-27 20:57:16 -0400

    Index local vars and pass
    Not declaring yet

--- Lbac.Compiler/CodeGenerator.fs
+++ Lbac.Compiler/CodeGenerator.fs
@@ -11,12 +11,12 @@
         | Divide -> instruction.Div
         | Assign -> failwith "Sorry; no can do"
 
-    let rec codegenExpr (acc : Method) (expr : Expr) = 
+    let rec codegenExpr (acc : Method) locals (expr : Expr) = 
         match expr with
         | Variable v -> Error "Sorry; no can do"
         | Invoke m -> Error "Sorry; no can do"
         | Minus e -> 
-            match codegenExpr acc e with
+            match codegenExpr acc locals e with
             | Success m -> Success({ m with Instructions = m.Instructions @ [Neg] })
             | err -> err
         | Number n -> 
@@ -24,8 +24,8 @@
             | 0 -> Success({ acc with Instructions = acc.Instructions @ [Ldc_I4_0] })
             | _ -> Success({ acc with Instructions = acc.Instructions @ [Ldc_I4 n] })
         | Binary (lhs, oper, rhs) -> 
-            let lhsMethod = codegenExpr { acc with Instructions = [] } lhs
-            let rhsMethod = codegenExpr { acc with Instructions = [] } rhs
+            let lhsMethod = codegenExpr { acc with Instructions = [] } locals lhs
+            let rhsMethod = codegenExpr { acc with Instructions = [] } locals rhs
             let operInst = codegen_oper oper
             match (lhsMethod, rhsMethod) with
                 | (Success l, Success r) -> 
@@ -35,11 +35,15 @@
                 | (Error l, _) -> lhsMethod
                 | (_, Error r) -> rhsMethod
 
-    let rec codegen (expList : ParseResult) =
+    let rec codegen (parsed : ParseResult) =
+        let locals = 
+            parsed.Locals 
+            |> List.ofSeq 
+            |> List.mapi (fun i l -> (i, l))
         let tryCodeGenLine acc line = 
             match acc, line with
-            | Success accMethod, Success expr -> codegenExpr accMethod expr
+            | Success accMethod, Success expr -> codegenExpr accMethod locals expr
             | _, Error err -> Error err
             | Error err, _ -> Error err
         let emptyMethod = Success( { Instructions = List.empty<instruction>; Locals = List.empty<LocalVar> } )
-        List.fold tryCodeGenLine emptyMethod expList.Lines
+        List.fold tryCodeGenLine emptyMethod parsed.Lines
