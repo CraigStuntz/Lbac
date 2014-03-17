087d077b5b76dab9a2ead5597df66ffccca859d0
CraigStuntz <craig_stuntz@acm.org>
2014-03-16 23:15:15 -0400

    Add a test for OptimizeIL
    More to come, I promise.

---
+++ Lbac.Tests/OptimizeILTests.fs
@@ -0,0 +1,14 @@
+﻿namespace Lbac.Tests
+
+open IL
+open OptimizeIL
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type OptimizeILTests() =
+
+    [<TestMethod>]
+    member x.``should change Ldc_I4 2 to LDC_I4_2`` () = 
+        let input = { Instructions = [Ldc_I4 2]; Locals = [] }
+        let actual = optimize input
+        Assert.AreEqual([Ldc_I4_2], actual.Instructions)
