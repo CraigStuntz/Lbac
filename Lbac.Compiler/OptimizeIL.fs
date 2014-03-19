f3daef581ac91e8a887fc6b54e7bdcaf518e932e
CraigStuntz <craig_stuntz@acm.org>
2014-03-18 22:06:55 -0400

    More optimize IL tests

--- Lbac.Compiler/OptimizeIL.fs
+++ Lbac.Compiler/OptimizeIL.fs
@@ -6,15 +6,15 @@
     let private maxByte = Convert.ToInt32 System.Byte.MaxValue
 
     let private optimalShortEncodingFor = function
-        | Ldc_I4  0   -> Ldc_I4_0 
-        | Ldc_I4  1   -> Ldc_I4_1 
-        | Ldc_I4  2   -> Ldc_I4_2 
-        | Ldc_I4  3   -> Ldc_I4_3 
-        | Ldc_I4  4   -> Ldc_I4_4 
-        | Ldc_I4  5   -> Ldc_I4_5 
-        | Ldc_I4  6   -> Ldc_I4_6 
-        | Ldc_I4  7   -> Ldc_I4_7 
-        | Ldc_I4  8   -> Ldc_I4_8 
+        | Ldc_I4 0    -> Ldc_I4_0 
+        | Ldc_I4 1    -> Ldc_I4_1 
+        | Ldc_I4 2    -> Ldc_I4_2 
+        | Ldc_I4 3    -> Ldc_I4_3 
+        | Ldc_I4 4    -> Ldc_I4_4 
+        | Ldc_I4 5    -> Ldc_I4_5 
+        | Ldc_I4 6    -> Ldc_I4_6 
+        | Ldc_I4 7    -> Ldc_I4_7 
+        | Ldc_I4 8    -> Ldc_I4_8 
         | Ldloc 0     -> Ldloc_0  
         | Ldloc 1     -> Ldloc_1  
         | Ldloc 2     -> Ldloc_2  
--- Lbac.Tests/OptimizeILTests.fs
+++ Lbac.Tests/OptimizeILTests.fs
@@ -12,3 +12,21 @@ type OptimizeILTests() =
         let input = { Instructions = [Ldc_I4 2]; Locals = [] }
         let actual = optimize input
         Assert.AreEqual([Ldc_I4_2], actual.Instructions)
+
+    [<TestMethod>]
+    member x.``should leave Stloc 100000 as Stloc 100000`` () = 
+        let input = { Instructions = [Ldc_I4_1; Stloc 100000; Ldloc 100000]; Locals = [] }
+        let actual = optimize input
+        Assert.AreEqual([Ldc_I4_1; Stloc 100000; Ldloc 100000], actual.Instructions)
+
+    [<TestMethod>]
+    member x.``should change Stloc 10 to Stloc_S 10`` () = 
+        let input = { Instructions = [Ldc_I4_1; Stloc 10; Ldloc 10]; Locals = [] }
+        let actual = optimize input
+        Assert.AreEqual([Ldc_I4_1; Stloc_S 10uy; Ldloc_S 10uy], actual.Instructions)
+
+    [<TestMethod>]
+    member x.``should change Stloc 1 to Stloc_1`` () = 
+        let input = { Instructions = [Ldc_I4_1; Stloc 1; Ldloc 1]; Locals = [] }
+        let actual = optimize input
+        Assert.AreEqual([Ldc_I4_1; Stloc_1; Ldloc_1], actual.Instructions)
